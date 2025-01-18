fa4ff3ec4a0667a73d9e80b11ad893e460f06f7c
RomanSkochko <romanslifestory@gmail.com>
2025-01-18 14:14:33 +0200

    add new project

---
+++ src/test/java/org/example/PatternBasics/PatternBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.PatternBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
